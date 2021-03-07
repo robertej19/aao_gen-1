992fd2c57b34a1cbc67de2e49de9e17588c4aca0
robertej19 <robertej@mit.edu>
2021-03-06 19:28:22 -0500

    pi0 wrapper is full functional v1 with aao_norad

--- gen_wrapper/src/pi0_gen_wrapper.py
+++ gen_wrapper/src/pi0_gen_wrapper.py
@@ -111,7 +111,7 @@ def gen_events(args,repo_base_dir):
     ratio = 0
 
     max_num_loops = args.maxloops
-    gen_rate = 0.001 #seconds per event for aao_norad
+    gen_rate = 0.0005 #seconds per event for aao_norad
     for loop_counter in range(0,max_num_loops+1):
         print("generating {} raw events".format(args.trig))
         gen_input_file(args)
