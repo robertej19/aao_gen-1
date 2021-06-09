4556d69bc6d292710f4029200a26c8a8fae01cf9
robertej19 <robertej@mit.edu>
2021-06-09 13:53:35 -0400

    typo

--- aao_gen.py
+++ aao_gen.py
@@ -134,7 +134,8 @@ def gen_events(args,repo_base_dir):
         ratio = compare_raw_to_filt(args,num_desired_events)
         
         #if abs(ratio-1) < args.precision/100:
-        if (ratio > 1):# and (abs(ratio-1) < args.precision/100): #This should be replaced to just truncate once the desired number of events are made
+        #if (ratio > 1):# and (abs(ratio-1) < args.precision/100): #This should be replaced to just truncate once the desired number of events are made
+        if abs(ratio-1) < args.precision/100:
         #if (ratio ==1):
             break
         elif loop_counter == max_num_loops:
---
+++ batch_generator.py
@@ -0,0 +1,6 @@
+#This is to make large scale simulation runs
+
+q2_divisions = [0.5,1,2,5,9,12]
+xb_divisions = [0,0.2,0.5,1]
+t_divisions = [0,1,2,3,6]
+phi_divisions = [0,90,270,360]
